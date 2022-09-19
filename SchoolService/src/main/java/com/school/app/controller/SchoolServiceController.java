526e52ed1216c0c23abf63836f2ab22ec37b18e7
sfjbssept <sfjbssept@gmail.com>
2022-09-19 10:13:03 +0000

    added school service

---
+++ SchoolService/src/main/java/com/school/app/controller/SchoolServiceController.java
@@ -0,0 +1,37 @@
+package com.school.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/schooldetail")
+public class SchoolServiceController {
+	@Autowired
+	RestTemplate restTemplate;
+	@GetMapping(value="/{schoolname}")
+	public String getStudents(@PathVariable("schoolname") String schoolname){
+		System.out.println("Getting School details for school: " + schoolname);
+		String requestUrl="http://localhost:9091/getStudentDetailsForSchool/"+schoolname;
+		
+		HttpHeaders headers=new HttpHeaders();
+		HttpEntity<String> httpEntity=new HttpEntity<String>(headers);
+		
+		
+		ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, httpEntity, String.class);
+		String student = response.getBody();
+		
+		System.out.println("Reponse received as "+student);
+		
+		return "School name :"+schoolname+" \n Student details :"+student;
+		
+	}
+
+}
