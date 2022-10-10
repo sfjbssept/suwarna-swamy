2f1acd44127fa2b459fb62c8d28090b237aef2bc
sfjbssept <sfjbssept@gmail.com>
2022-10-10 07:40:08 +0000

    addinf Componentsuserform

--- Angular-Training/starter-app/src/app/app.component.ts
+++ Angular-Training/starter-app/src/app/app.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username :String= "";
-  salary:number=0;
+  age:number=0;
   title = 'Hello World';
   desc: String="AngularJS is an open-source web application framework. It was originally developed in 2009 by Misko Hevery and Adam Abrons. It is now maintained by Google. Its latest version is 1.2.21. AngularJS is a structural framework for dynamic web applications."
 
 
   save(){
     console.log("The user name is "+
-    this.username  + " who's salary is "+ this.salary);
+    this.username  + " who's salary is "+ this.age);
   
   }
 }
