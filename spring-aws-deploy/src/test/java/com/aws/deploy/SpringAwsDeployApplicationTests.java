bae44fe54713933a80e3ff5837072c469d246034
sfjbssept <sfjbssept@gmail.com>
2022-10-26 05:16:59 +0000

    serverless app deploy on cloud

---
+++ spring-aws-deploy/src/test/java/com/aws/deploy/SpringAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
