c2beedc7373e139493ab1efc716f5da2caf1871d
sfjbssept <sfjbssept@gmail.com>
2022-11-11 06:32:36 +0000

    gateway for usecase

---
+++ gateway-usecase-service/src/main/java/com/gate/GatewayUsecaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayUsecaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayUsecaseServiceApplication.class, args);
+	}
+
+}
---
+++ gateway-usecase-service/src/main/java/com/gate/config/GatewayConfig.java
@@ -0,0 +1,17 @@
+package com.gate.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class GatewayConfig {
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTeamplate()
+	{
+		return new RestTemplate();
+
+}
+}
